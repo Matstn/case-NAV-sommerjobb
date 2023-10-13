7b5210424fe2c7e737215ba0f66e2d3a82abf6a6
matstn <matstn@stud.ntnu.no>
2023-10-13 17:23:13 +0200

    Fixed console print message

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,15 +5,16 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        User user = new User(420000, 450000, 560000);
+        User user = new User(420, 450, 560);
         Calculations calculations = new Calculations(user);
 
         if(!calculations.isEligibleForDagpenger()){
-            System.out.println("Du kan dessverre ikke motta dagpenger.");
+            System.out.println("Du kan dessverre ikke motta dagpenger, fordi du ikke har tjent nok i løpet av de tre foregående årene.");
         }
         
-        System.out.println("Gratulerer, du kan motta dagpenger! Du kan motta " + calculations.getDagSats() +"kr som dagsats!");
-
+        else{
+            System.out.println("Gratulerer, du kan motta dagpenger! Du kan motta " + calculations.getDagSats() +"kr som dagsats!");
+        }
 
     }
     
